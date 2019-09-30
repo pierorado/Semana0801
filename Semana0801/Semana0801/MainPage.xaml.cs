c5fcb8a923dced5f42e4c60e8c2ed2b2e98893a9
delpiero ramos ramos <delpiee@hotmail.com>
2019-09-30 15:35:59 -0500

    Agregar archivos de proyecto.

---
+++ Semana0801/Semana0801/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana0801
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += Item1_Clicked;
+            Item2.Clicked += Item2_Clicked;
+            Item3.Clicked += Item3_Clicked;
+            Item4.Clicked += Item4_Clicked;
+            Item5.Clicked += Item5_Clicked;
+            
+        }
+
+        private void Item5_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Item4_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Item3_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Item2_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Item1_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Semana0801/Semana0801/TabbedPage1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana0801
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : ContentPage
+    {
+        public TabbedPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
